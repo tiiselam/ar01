d4b9fead23e1cf44c13ace76af79471e2a61a21e
jcfernandezz <jcfernandezz@gmail.com>
2017-09-18 09:21:50 -0400

    corrige error. Generaba proveedores con status indeterminado

--- SQL/40_SP_PBEGP_Archivo_Proveedores.sql
+++ SQL/40_SP_PBEGP_Archivo_Proveedores.sql
@@ -4,9 +4,10 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
---27/7/17 jcf zipcode es obligatorio. Indicar cuit si no tiene num ingresos brutos.
---29/8/17 jcf quita caracteres especiales de dirección
---14/9/17 jcf comenta proveedores de tblPBE301
+--27/07/17 jcf zipcode es obligatorio. Indicar cuit si no tiene num ingresos brutos.
+--29/08/17 jcf quita caracteres especiales de dirección
+--14/09/17 jcf comenta proveedores de tblPBE301
+--18/09/17 jcf Agrega filtro PBE_Estatus>0
 
 alter procedure [dbo].[SP_PBEGP_Archivo_Proveedores]
 @compania as bigint,
@@ -15,16 +16,22 @@ as
 begin
 	delete from tblpbe999;
 
+	--Cabecera
 	insert into tblpbe999 (id,txtfield)
-	select '0','FH,'+													---- campo 1 Tipo de Registro
+	select '0','FH,'+													--- campo 1 Tipo de Registro
 		replace(convert(varchar(19),@fecha,103),'/','')+','+			--- campo 2 fecha archivo
-		'PCBE,'+	----- campo 3 Objeto. De uso para HSBC
-		ltrim(str((select count(*) from tblPBE001 where PBE_generado=0)+
-		(select count(*) from tblPBE001 where PBE_generado=0 and PBE_IdAutorizado<>'')+
-		(select count(*) from tblPBE301 where PBE_generado=0))+1)+','+	---- campo 4 Cantidad de Registros del archivo
-		'A,C,AR,HBAR,'+	----campo 5 Tipo de Autorización, 6 Tipo de Archivo,7 País Cliente. De uso en HSBC,8 Banco del Cliente. De uso en HSBC
-		rtrim(replace(left(c.TAXREGTN,12),'-',''))+',,'	----- campo 9 Identificación del Cliente, 10 Nombre del archivo
-	from DYNAMICS.dbo.SY01500 c where c.CMPANYID=@compania
+		'PCBE,'+														--- campo 3 Objeto. De uso para HSBC
+		ltrim(str(
+			1+(select count(*) from tblPBE001 where PBE_generado=0 and PBE_Estatus>0)
+			--(select count(*) from tblPBE001 where PBE_generado=0 and PBE_IdAutorizado<>'')+
+			--(select count(*) from tblPBE301 where PBE_generado=0)
+			))+','+														--- campo 4 Cantidad de Registros del archivo
+		'A,C,AR,HBAR,'+													--- campo 5 Tipo de Autorización, 6 Tipo de Archivo,7 País Cliente. De uso en HSBC, 8 Banco del Cliente. De uso en HSBC
+		rtrim(replace(left(c.TAXREGTN,12),'-',''))+',,'					--- campo 9 Identificación del Cliente, 10 Nombre del archivo
+	from DYNAMICS.dbo.SY01500 c 
+	where c.CMPANYID=@compania
+
+	--Registro del proveedor
 	insert into tblpbe999 (id,txtfield)
 	select p.VENDORID,'SP,I,'+											--- CAMPO 1 Tipo de Registro,	CAMPO 2 Indicador de Template. Uso HSBC
 		case PBE_Estatus when 1 then 'U' when 2 then 'D' end+','+		--- CAMPO 3 Tipo de Acción
@@ -51,23 +58,29 @@ begin
 	left join DYNAMICS.dbo.AWLI40330 m on m.RESP_TYPE=r.RESP_TYPE
 	left join SY01200 e on e.Master_Type='VEN' and e.Master_ID=d.VENDORID  
 	 where d.PBE_generado=0
+	   and d.PBE_Estatus>0
+
+	--Registro del autorizado
 	insert into tblpbe999 (id,txtfield)
-	select p.VENDORID,'SP,I,'+	--- campo 1	Tipo de Registro, campo 2	Indicador de Template. Uso HSBC
+	select p.VENDORID,'SP,I,'+											--- campo 1	Tipo de Registro, campo 2	Indicador de Template. Uso HSBC
 		case PBE_Estatus when 1 then 'U,' when 2 then 'D,' end+			----campo 3	Tipo de Acción
 		rtrim(substring(p.TXRGNNUM,1,11))+','+							--- campo 4	CUIT Proveedor
 		replace(rtrim(left(p.VENDNAME,40)),',','')+','+					---- campo 5	Razón Social/Nombre Proveedor
 		replace(replace(replace(rtrim(left(p.ADDRESS1+' '+p.ADDRESS2+' '+p.ADDRESS3,40)),',','') , 'º', '' ), '"', '')+','+	---campo 6	Domicilio Proveedor
 		replace(rtrim(left(p.CITY,20)),',','')+','+						--- campo 7	Localidad Proveedor
-		','+	---- campo 8	Código Postal del Proveedor
-		','+	--- campo 9		Provincia Proveedor
+		','+															---- campo 8	Código Postal del Proveedor
+		','+															--- campo 9		Provincia Proveedor
 		rtrim(left(pbe_sucursal,3))+','+								--- campo 10	Sucursal de Entrega de Cheques
-		','+	--- campo 11	Número de Ingresos Brutos Proveedor
+		','+															--- campo 11	Número de Ingresos Brutos Proveedor
 		replace(rtrim(left(d.PBE_CondicionIVA,25)),',','')+',,,,'+		--- campo 12	Condición de IVA,13	Número de Teléfono Proveedor, 14 E-mail proveedor, 15 Nombre del Contacto
 		case d.pbe_tipoid when 1 then '50' when 2 then '52' when 3 then '53' when 4 then '54' end+rtrim(left(d.PBE_IdAutorizado,8))+','+	--- campo 16 Tipo y Número de documento del Autorizado
 		replace(rtrim(left(d.PBE_NombreAutorizado,40)),',','')+','		--- campo 17 Nombre del Autorizado
 	from tblPBE001 d
 	left join PM00200 p on p.VENDORID=d.VENDORID
-	where d.PBE_generado=0 and d.PBE_IdAutorizado<>''
+	where d.PBE_generado=0 
+	and d.PBE_IdAutorizado<>''
+	and d.PBE_Estatus>0
+
 	--union
 	--select p.VENDORID,'SP,I,'+	--- campo 1	Tipo de Registro, campo 2	Indicador de Template. Uso HSBC
 	--	case PBE_Estatus when 1 then 'U,' when 2 then 'D,' end+			----campo 3	Tipo de Acción
